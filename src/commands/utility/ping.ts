4c810243a9d9db0930765e27d98eaafef8245c7e
CL TheDreWen <louis.charneux@gmail.com>
2025-10-28 11:44:54 +0100

    Set ping cmd for user mp and other.

--- src/commands/utility/ping.ts
+++ src/commands/utility/ping.ts
@@ -1,9 +1,18 @@
-import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { ApplicationIntegrationType, CommandInteraction, InteractionContextType, SlashCommandBuilder } from "discord.js";
 
 export default {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Pong again!'),
+        .setDescription('Pong again!')
+        .setIntegrationTypes(
+            ApplicationIntegrationType.GuildInstall,
+            ApplicationIntegrationType.UserInstall
+        )
+        .setContexts(
+            InteractionContextType.BotDM,
+            InteractionContextType.Guild,
+            InteractionContextType.PrivateChannel
+        ),
     async execute(interaction : CommandInteraction) {
         await interaction.reply('Pong !');
     }
